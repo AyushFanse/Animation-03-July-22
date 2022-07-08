df925d0edcd2e0ba427744881a05d5c9782a6969
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2022-07-08 11:35:33 +0530

    Animation

--- script.js
+++ script.js
@@ -44,6 +44,20 @@ heart.onclick = () => {
 //*----------------------- Flip Card -----------------------*//
 let flip = document.querySelector(".FlipOut");
 
-const Flip = ()=>{
-    flip.classList.toggle("active");
+const Flip = () => {
+  flip.classList.toggle("active");
+};
+
+//*-----------------------------
+
+document.querySelector("body").addEventListener("mousemove", eyeball);
+function eyeball() {
+  let eye = document.querySelectorAll(".eye i");
+  eye.forEach(function (eye) {
+    let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+    let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let rot = radian * (188 / Math.PI) * -1 + 270;
+    eye.style.transform = "rotate(" + rot + "deg)";
+  });
 }
